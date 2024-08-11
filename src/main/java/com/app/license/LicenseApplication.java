babb8c3e78a62e239b5e4b6c5f9363b17ba72fd0
stevensameh15 <stevensameh15@gmail.com>
2024-08-11 15:54:27 +0300

    SecurityConfig.java is removed

--- src/main/java/com/app/license/LicenseApplication.java
+++ src/main/java/com/app/license/LicenseApplication.java
@@ -3,7 +3,9 @@ package com.app.license;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class LicenseApplication {
 
 	public static void main(String[] args) {
